3137099215edf3dcd4869571701c936a75027e85
Churakov Pavel <131313pasha@gmail.com>
2025-03-19 11:13:45 +0300

    routing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
-// index.tsx - entry point
-
 import {createRoot} from 'react-dom/client';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import {DiagramNode} from '~shared';
 import './index.css';
 import {App} from './app';
@@ -10,4 +9,11 @@ const a = {} as DiagramNode;
 console.log('Hello, world!', a);
 
 const root = createRoot(document.getElementById('root') as HTMLDivElement);
-root.render(<App />);
+root.render(
+  <Router>
+    <Routes>
+      <Route key='/functions' path='/functions' element={<div />} />
+      <Route key='/' path='/' element={<App />} />
+    </Routes>
+  </Router>,
+);
